0821a20defffc3ffb5d3f484159d0ca01a4cd307
RyanDBarnett <conner2468@gmail.com>
2021-02-15 23:03:01 -0700

    Add item validations

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Item, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
-    it { should validate_numericality_of :unit_price }
+    it { should validate_numericality_of(:unit_price).is_greater_than(0) }
   end
 end
