c0e9b9e267b14317a0b1832b2362272e08572531
RyanDBarnett <conner2468@gmail.com>
2021-02-15 22:49:36 -0700

    Add Item validation tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
-class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+RSpec.describe Item, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_numericality_of :unit_price }
+  end
 end
