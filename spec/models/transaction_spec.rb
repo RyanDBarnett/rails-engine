21ec4efa39f6faa4b8a7dde294a466e21850efcb
RyanDBarnett <conner2468@gmail.com>
2021-02-15 23:14:43 -0700

    Add transaction validations

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
-class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+RSpec.describe Transaction, type: :model do
+  describe 'validations' do
+    it { should validate_numericality_of(:credit_card_number).only_integer }
+    it { should validate_presence_of :credit_card_expiration }
+    it { should validate_presence_of :result }
+  end
 end
