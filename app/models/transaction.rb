14fd5b3d806ab5bba5e2d6fbef86ee3146cc6122
RyanDBarnett <conner2468@gmail.com>
2021-02-15 23:28:34 -0700

    Add transaction includes success or failed validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
   validates_numericality_of :credit_card_number, only_integer: true
   validates_presence_of :credit_card_expiration
-  validates_presence_of :result
-  
+  validates :result, presence: true, inclusion: ['success', 'failed']
+
   belongs_to :invoice
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Transaction, type: :model do
     it { should validate_numericality_of(:credit_card_number).only_integer }
     it { should validate_presence_of :credit_card_expiration }
     it { should validate_presence_of :result }
+    it { should validate_inclusion_of(:result).in_array(['success', 'failed']) }
   end
 end
